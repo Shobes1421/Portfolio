9bb6a976d0b3d0a6e739d78260effe8cc369c55d
David Shober <shobes1421@hotmail.com>
2020-08-09 14:00:33 -0600

    Updates

---
+++ precache-manifest.26356fd49215b7a3579839f73d6a064e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac094263290e1defbee437eb6a6ff7f4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "523354817159437adcd6",
+    "url": "/Portfolio/static/css/main.59ece434.chunk.css"
+  },
+  {
+    "revision": "076442782ce0e4844c4a",
+    "url": "/Portfolio/static/js/2.117e356d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.117e356d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523354817159437adcd6",
+    "url": "/Portfolio/static/js/main.369b29b7.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "a87c4fb6a2f166aabd75e242c33ca8ef",
+    "url": "/Portfolio/static/media/Budget-Tracker.a87c4fb6.png"
+  },
+  {
+    "revision": "d8c45fc43f4afacd7083a278840c7036",
+    "url": "/Portfolio/static/media/Dayplannerpic.d8c45fc4.jpeg"
+  },
+  {
+    "revision": "0733e01a65f49af9e540097c9ce457f8",
+    "url": "/Portfolio/static/media/Fitness-Tracker.0733e01a.png"
+  },
+  {
+    "revision": "8d988276205b2a6f3de69fef4cae96c7",
+    "url": "/Portfolio/static/media/IMG_0029.8d988276.jpg"
+  },
+  {
+    "revision": "3736770e6b1688b283fc343b77fca8c5",
+    "url": "/Portfolio/static/media/Note-Taker.3736770e.png"
+  },
+  {
+    "revision": "d9cb21adb7a3da907ae35d1846476012",
+    "url": "/Portfolio/static/media/Weather Dashboard.d9cb21ad.png"
+  },
+  {
+    "revision": "813b686fcec22dd4deb53b53fd38f0c7",
+    "url": "/Portfolio/static/media/the-sloshed-guide-screenshot.813b686f.png"
+  }
+]);
