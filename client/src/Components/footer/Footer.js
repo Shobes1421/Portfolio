7dcc6dfca4c24202984b5285e96d989e87c22a4a
David Shober <shobes1421@hotmail.com>
2020-08-10 21:43:36 -0600

    make it better

--- client/src/Components/footer/Footer.js
+++ client/src/Components/footer/Footer.js
@@ -5,7 +5,7 @@ import './style.css';
 function Footer() {
     return (
         <footer className="container-fluid navbar justify-content-center fixed-bottom">
-            <p >Copyright &copy; 2020</p>
+            <p >Copyright &copy; 2020 David Shober</p>
         </footer>
     )
 }
